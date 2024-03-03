32a122e4a328c9edf61b9172c2204579bd7ac3fe
David URVOY <dev@david.urvoy.email>
2024-03-03 19:52:04 +0100

    portal materials as react components

--- portal/src/world/Portal-Journey.tsx
+++ portal/src/world/Portal-Journey.tsx
@@ -1,6 +1,6 @@
 import { Float, Text, useGLTF, useTexture } from '@react-three/drei'
 import { GroupProps } from '@react-three/fiber'
-import { MeshBasicMaterial, MeshStandardMaterial, SRGBColorSpace } from 'three'
+import { SRGBColorSpace } from 'three'
 import { NestedObjectMap } from '../types/glb-types'
 import Fireflies from './Fireflies'
 import { PortalShader } from './PortalShader'
@@ -12,32 +12,22 @@ export default function PortalJourney(props: GroupProps) {
 	texture.flipY = false
 	texture.colorSpace = SRGBColorSpace
 
-	const poleLightMaterial = new MeshBasicMaterial({ color: 0xffffe5 })
+	const PoleLightMaterial = () => <meshBasicMaterial color='#ffffe5' />
 
 	return <group {...props} dispose={null}>
 		<Float><Text position-y={3}>Three Journey</Text></Float>
 		<Fireflies />
-		<mesh
-			geometry={nodes.poleLightA.geometry}
-			material={poleLightMaterial}
-			position={[0.715, 1.066, 0.234]}
-		/>
-		<mesh
-			geometry={nodes.portalLight.geometry}
-			position={[0, 0.851, -1.774]}
-			rotation={[Math.PI / 2, 0, 0]}
-		>
+		<mesh geometry={nodes.poleLightA.geometry} position={[0.715, 1.066, 0.234]}>
+			<PoleLightMaterial />
+		</mesh>
+		<mesh geometry={nodes.portalLight.geometry} position={[0, 0.851, -1.774]} rotation={[Math.PI / 2, 0, 0]}>
 			<PortalShader />
 		</mesh>
-		<mesh
-			geometry={nodes.poleLightB.geometry}
-			material={poleLightMaterial}
-			position={[-0.642, 1.066, 0.234]}
-			rotation={[Math.PI, 0, Math.PI]}
-		/>
-		<mesh
-			geometry={nodes.baked.geometry}
-			material={new MeshStandardMaterial({ map: texture })}
-		/>
+		<mesh geometry={nodes.poleLightB.geometry} position={[-0.642, 1.066, 0.234]} rotation={[Math.PI, 0, Math.PI]}>
+			<PoleLightMaterial />
+		</mesh>
+		<mesh geometry={nodes.baked.geometry}>
+			<meshStandardMaterial map={texture} />
+		</mesh>
 	</group>
 }
--- portal/src/world/Portal.tsx
+++ portal/src/world/Portal.tsx
@@ -1,6 +1,5 @@
 import { useGLTF, useTexture } from '@react-three/drei'
 import { GroupProps } from '@react-three/fiber'
-import { MeshStandardMaterial } from 'three'
 import { NestedObjectMap } from '../types/glb-types'
 import Fireflies from './Fireflies'
 import { ObjectLights } from './Lights'
@@ -14,26 +13,17 @@ export default function Portal(props: GroupProps & { unwrappedPath: string }) {
 	return <group {...props} dispose={null}>
 		<ObjectLights nodes={nodes} />
 		<Fireflies />
-		<mesh
-			geometry={nodes.Wood.geometry}
-			material={new MeshStandardMaterial({ map: texture })}
-			position={[1.2, 0.001, 1.449]}
-		/>
-		<mesh
-			geometry={nodes.Rock.geometry}
-			material={new MeshStandardMaterial({ map: texture })}
-			position={[1.385, 0.001, -1.321]}
-			rotation={[-Math.PI, 1.198, -Math.PI]}
-		/>
-		<mesh
-			geometry={nodes.Metal.geometry}
-			material={new MeshStandardMaterial({ map: texture })}
-			position={[1.135, 0.177, 0.802]}
-			rotation={[2.275, -1.134, 2.259]}
-		/>
-		<mesh
-			geometry={nodes.Grass.geometry}
-			material={new MeshStandardMaterial({ map: texture })}
-		/>
+		<mesh geometry={nodes.Wood.geometry} position={[1.2, 0.001, 1.449]} >
+			<meshStandardMaterial map={texture} />
+		</mesh>
+		<mesh geometry={nodes.Rock.geometry} position={[1.385, 0.001, -1.321]} rotation={[-Math.PI, 1.198, -Math.PI]}>
+			<meshStandardMaterial map={texture} />
+		</mesh>
+		<mesh geometry={nodes.Metal.geometry} position={[1.135, 0.177, 0.802]} rotation={[2.275, -1.134, 2.259]}>
+			<meshStandardMaterial map={texture} />
+		</mesh>
+		<mesh geometry={nodes.Grass.geometry}>
+			<meshStandardMaterial map={texture} />
+		</mesh>
 	</group>
 }
